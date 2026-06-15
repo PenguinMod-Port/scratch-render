65b7a630914db78c59ea82b0dbf7e2ba434ab3d7
jwklong <jwklong@gmx.com>
2026-06-15 22:36:25 +0100

    fix difference

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -2404,10 +2404,10 @@ class RenderWebGL extends EventEmitter {
                 case Drawable.BlendMode.MULTIPLICATIVE: blendInfo = [gl.FUNC_ADD, gl.DST_COLOR, gl.ONE_MINUS_SRC_ALPHA]; break;
                 case Drawable.BlendMode.SUBTRACTIVE: blendInfo = [gl.FUNC_REVERSE_SUBTRACT, gl.ONE, gl.ONE]; break;
                 case Drawable.BlendMode.SCREEN: blendInfo = [gl.FUNC_ADD, gl.ONE, gl.ONE_MINUS_SRC_COLOR]; break;
-                case Drawable.BlendMode.DIFFERENCE: blendInfo = [gl.FUNC_SUBTRACT, gl.ONE, gl.ONE]; break;
+                case Drawable.BlendMode.DIFFERENCE: blendInfo = [gl.FUNC_ADD, gl.ONE_MINUS_DST_COLOR, gl.ONE_MINUS_SRC_COLOR, gl.ONE, gl.ONE]; break;
             }
             gl.blendEquation(blendInfo[0]);
-            gl.blendFunc(blendInfo[1], blendInfo[2]);
+            gl.blendFuncSeparate(blendInfo[1], blendInfo[2], blendInfo[3] ?? blendInfo[1], blendInfo[4] ?? blendInfo[2]);
 
             twgl.setUniforms(currentShader, uniforms);
             twgl.drawBufferInfo(gl, this._bufferInfo, gl.TRIANGLES);
